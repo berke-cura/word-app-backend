f8d4eb68c116d58f99670dfa32e73389e6c458aa
Süleyman Ali Akpınar <suleyman@linkadoo.me>
2022-04-02 16:53:10 +0300

    checkCondition helper created

---
+++ api/helpers/checkCondition.js
@@ -0,0 +1,12 @@
+module.exports = async (word, existingLetters, nonExistingLetters, letterPlacements) => {
+    const wordArray = word.split("")
+    const vals = letterPlacements.map((item) => {
+        if (wordArray[(item.place - 1)] === item.letter
+            && existingLetters.every(item => wordArray.includes(item)
+                && !nonExistingLetters.some(item => wordArray.includes(item)))) {
+            return true
+        }
+    })
+    let checker = vals.every(v => v === true);
+    return checker
+};
