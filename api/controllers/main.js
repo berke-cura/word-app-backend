8a3f8430f74366494ce2b4666f74c6c3f6f1a07c
Süleyman Ali Akpınar <suleyman@linkadoo.me>
2022-04-11 05:38:18 +0300

    fix

--- api/controllers/main.js
+++ api/controllers/main.js
@@ -9,7 +9,6 @@ exports.main = async (req, res) => {
     const doc = require(`../../data/en/en${  letterCount}`);
     const array = []
 
-
     await Promise.all(doc.values.map(async (word) => {
         const isOkey = await checkCondition(word, existingLetter, notExistingLetter, letterPlace)
         if (isOkey) {
@@ -20,23 +19,3 @@ exports.main = async (req, res) => {
 
     res.status(200).json({ message: 'done', words: array });
 }
-
-// REQUEST BODY EXAMPLE
-/* {
-    "letterCount" : "5",
-    "existingLetter": ["a","s","d"],
-    "notExistingLetter": ["1","2","3"],
-    "letter_place": {"a":3, "d":5}
-} */
-
-    // TODOS
-    // get data by letterCount
-    // ("data/en/en" + layerCount) yaparak dosya okuyacak
-    // dosyada ki bütün kelimeleri loop edecek
-        // her iterasyonda
-            // layerPlace var mı bakacak
-            // existingLetter var mı bakacak
-            // nonExistingLetter var mı bakacak
-                // Eğer tüm koşullar istenildiği gibiyse
-                    // bu kelime response datasının tutulduğu bir array e eklenecek
-    // loop un sonunda response olarak bu kelime array i dönecek
--- api/helpers/checkCondition.js
+++ api/helpers/checkCondition.js
@@ -1,25 +1,10 @@
-/* eslint-disable consistent-return */
-/* eslint-disable no-shadow */
-/* eslint-disable array-callback-return */
 module.exports = async (word, existingLetters, nonExistingLetters, letterPlacements) => {
     const wordArray = word.split("")
-
-    const vals = letterPlacements.map((item) => {
-        
-        if(letterPlacements.length > 0) {
-            if (wordArray[(item.place - 1)] === item.letter
-            && existingLetters.every(item => wordArray.includes(item)
-                && !nonExistingLetters.some(item => wordArray.includes(item)))) {
-            return true
-          }
-        }
-        else if( existingLetters.every(item => wordArray.includes(item)  && !nonExistingLetters.some(item => wordArray.includes(item)))) {
-                return true
-              }
-       
-    })
-
-
-    const checker = vals.every(v => v === true);
-    return checker
+  const nonExistingFlag = !wordArray.some(r=> nonExistingLetters.indexOf(r) >= 0)
+  const existingFlag = existingLetters.every((v) => wordArray.indexOf(v) >= 0)
+  const letterPlacementDummy = letterPlacements.filter((i) => {
+    if (wordArray.indexOf(i.letter) === (i.place -1)) return true;
+  })
+  const letterPlacementFlag = letterPlacementDummy.length === letterPlacements.length;
+  if(nonExistingFlag && existingFlag && letterPlacementFlag) return true
 };
