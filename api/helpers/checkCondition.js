a4c81ea684494f9ae9fbea3306569d18ef739131
berke-cura <berkecura@protonmail.com>
2022-04-11 11:03:42 +0300

    checkCondition 6: (i.place) --> -1 deleted

--- api/helpers/checkCondition.js
+++ api/helpers/checkCondition.js
@@ -1,9 +1,9 @@
 module.exports = async (word, existingLetters, nonExistingLetters, letterPlacements) => {
-    const wordArray = word.split("")
+  const wordArray = word.split("")
   const nonExistingFlag = !wordArray.some(r=> nonExistingLetters.indexOf(r) >= 0)
   const existingFlag = existingLetters.every((v) => wordArray.indexOf(v) >= 0)
   const letterPlacementDummy = letterPlacements.filter((i) => {
-    if (wordArray.indexOf(i.letter) === (i.place -1)) return true;
+    if (wordArray.indexOf(i.letter) === (i.place)) return true;
   })
   const letterPlacementFlag = letterPlacementDummy.length === letterPlacements.length;
   if(nonExistingFlag && existingFlag && letterPlacementFlag) return true
